5930b9a2b6baa7eee50516756d95137843d3058f
Manu <116085483+theperk08@users.noreply.github.com>
2023-06-27 14:47:05 +0200

    Update test_dash2.py

--- test_dash2.py
+++ test_dash2.py
@@ -54,7 +54,7 @@ app.layout = html.Div(
     children=[
         html.Br(),
         html.Img(
-            src="/assets/logo_enedis.png",
+            src="assets/logo_enedis.PNG",
             style={'height': '200px'}
         ),
         html.Br(),
